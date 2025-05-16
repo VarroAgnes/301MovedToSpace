804d9f79b6d668c651dbd46511f0645dcee4da72
Axi20 <varagn19@gmail.com>
2025-05-16 23:16:33 +0200

    Add homepage and css

---
+++ moved-to-space/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+const Homepage = () => {
+    return (
+        <>
+            <div className="stars"></div>
+            <div className="center-modal">
+                <h1>Hello</h1>
+            </div>
+        </>
+      );
+  };
+  
+  export default Homepage;
+  
